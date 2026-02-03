5469fa45dc498360f5c1a00c30e2359eec163186
Connor Hull <chull4704@gmail.com>
2026-02-02 20:50:38 -0500

    Initial setup of Router, Database, and Server Connection

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # To-Do-List-Backend
 This is a very small personal project that is meant to practice basic capabilities of node and express. Also to practice general workflow of web applications.
 
+Language - JavaScript
+Framework - Node.js, Express.js
+Database - MongoDB
+
+Dependencies - see package.json 
+    (to install dependencies 'npm install' from package.json)
+
+TO-DO-LIST APPLICATION (BACKEND): 
+
+DATA MODEL STRUCTURE:
+Task ID
+Task Title
+Task Description
+Completion Status
+Time of Creation
+Time of Update
+User ID
+
+ROUTES:
+GET /tasks : fetch all tasks
+POST /tasks : create a new task
+PUT /tasks/:id : update a task (completion, text edit)
+DELETE /tasks/:id : delete a task
 
--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
 
-const app = express()
+const app = express();
+const router = express.Router();
+
+configDotenv('./.env');
+const PORT = process.env.PORT;
+const db = process.env.MONGO_URI;
+
+router.get('/', (req, res, next) => {
+  console.log("Router is working");
+  res.send("Hello");
+  res.end();
+});
+
+app.use(router);
+
+async function startServer() {
+  try {
+    await mongoose.connect(db);
+    console.log("Connected to MongoDB Database");
+
+    app.listen(PORT, (err) => {
+      if (err) return console.log("Server Error Occurred");
+      console.log(`Express Server Listening on port ${PORT}`);
+    });
+  } catch (error) {
+    return console.log("Failed to Connect to Database")
+  }
+};
+
+startServer()
